ab701898e953eb3dbdf0c2dae794c13f95a3bfe8
shehabSh <shehab.e.sh@gmail.com>
2023-04-10 00:05:55 +0300

    new update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Task;
 class TaskController extends Controller
 {
     //
@@ -10,21 +12,32 @@ class TaskController extends Controller
     public function index()
     {
         # code...
-        $tasks = DB::table('tasks')->get();
+        // $tasks = DB::table('tasks')->get();
         // $task = new Task;
-//ببعت الداتا للفيو من خلال الكومباكت
-        return view('tasks' , compact('tasks'));
+        $tasks = Task::all();
+        //ببعت الداتا للفيو من خلال الكومباكت
+        return view('tasks', compact('tasks'));
     }
 
 
     public function store(Request $request)
     {
-        # code...
-         DB::table('tasks')->insert([
-            'name' => $request->name,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+             //debug
+        //dd($request);
+        // $task = new Task;
+        // $task->name = $request->name;
+        // $task->save;
+
+        // # code...
+        // DB::table('tasks')->insert([
+        //     'name' => $request->name,
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
+
+        Task::create([
+            'name' => $request->name
+        ]); 
 
         return redirect()->back();
     }
@@ -34,15 +47,16 @@ class TaskController extends Controller
 
     public function delete($id)
     {
-        DB::table('tasks')->where('id', $id)->delete();
+        // DB::table('tasks')->where('id', $id)->delete();
 
         // $task = Task::where('id', $id)->get();
 
         //لما بدي احذف فقط بدون ما استخدم اي شروطبعطيه ال id
-        //$task = Task::find($id) -> get();
+
+           $task = Task::find($id);
+
+           $task->delete();
 
         return redirect()->back();
     }
-
-    
 }
